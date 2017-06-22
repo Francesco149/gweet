b5839fcf459c8f5f07ade8f21a1193e7f0a31d8a
Franc[e]sco <lolisamurai@tfwno.gf>
2017-06-22 05:45:07 +0200

    release.sh: prettier output

--- release.sh
+++ release.sh
@@ -2,16 +2,16 @@
 
 git pull origin master
 
-echo "Compiling and Stripping"
+echo -e "\nCompiling and Stripping"
 go build --ldflags "-linkmode external -extldflags '-static -s -w'"
 
-echo "Packaging"
+echo -e "\nPackaging"
 folder="gweet-linux-$(uname -m)"
 mkdir -p "$folder"
 mv ./gweet $folder/gweet
 rm "$folder".tar.xz
 tar -cvJf "$folder".tar.xz "$folder"/gweet
 
-echo "Result:"
+echo -e "\nResult:"
 tar tf "$folder".tar.xz
 
