9b03cb429b1ca627c8d8f497a6d056ba346d2305
Franc[e]sco <lolisamurai@tfwno.gf>
2017-06-24 00:24:08 +0200

    release.sh: force PIE off

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@
 git pull origin master
 
 echo -e "\nCompiling and Stripping"
-LDFLAGS="$LDFLAGS -static -s -w -Wl,--gc-sections"
+LDFLAGS="$LDFLAGS -static -s -w -no-pie -Wl,--gc-sections"
 go build --ldflags "-linkmode external -extldflags '$LDFLAGS'"
 
 echo -e "\nPackaging"
